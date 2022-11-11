c1ddb0e783eff249b9535b48e9fb4d08a65ce573
margielajake <campbell.jake14@gmail.com>
2022-11-11 23:04:34 +0100

    new-changes

---
+++ index.js
@@ -0,0 +1,34 @@
+const selectElement = selector => {
+   const element = document.querySelector(selector)
+   if (element) return element;
+   throw new Error (`something went wrong, make sure that ${selector} exists.`)
+};
+
+const scrollHeader = () => {
+   const headerElement = selectElement('#header');
+   if(this.scrollY >= 15){
+      headerElement.classList.add('activated');
+   }else{
+      headerElement.classList.remove('activated');
+   }
+
+};
+
+window.addEventListener('scroll', scrollHeader);
+
+const menuToggleIcon =selectElement('#menu-toggle-icon')
+
+const toggleMenu = () => {
+   const mobileMenu = selectElement('#menu');
+   mobileMenu.classList.toggle('activated')
+   menuToggleIcon.classList.toggle('activated')
+};
+
+menuToggleIcon.addEventListener('click', toggleMenu);
+
+const bodyElement = document.body;
+const themeToggleBtn = selectElement('#theme-toggle-btn');
+
+themeToggleBtn.addEventListener('click', () => {
+   bodyElement.classList.toggle('dark-theme')
+})
